c58ac90cbf0cf4872fc417f32bab2b3fe4fc8f6f
Matheus Alencar <matheus@kleros.io>
2020-03-24 15:06:19 +0100

    fix: expand rules of list submission

--- bots/submit-list.js
+++ bots/submit-list.js
@@ -1,9 +1,35 @@
+const ethers = require('ethers')
+const { NO_LIST_SUBMITTED } = require('../utils/db-keys')
+
+// Used to ensure addresses are in checksummed format.
+const { getAddress } = ethers.utils
+
+// Submits an empty list of transactions if all conditions
+// below are met:
+// - We passed LIST_SUBMISSION_THRESHOLD_SECONDS;
+// - The session is not over;
+// - None lists were submitted by SUBMITTER_ADDRESSES;
+// - The alarm is not disarmed for this session.
 module.exports = async ({
   governor,
   lastApprovalTime,
   submissionTimeout,
-  currentSessionNumber
+  currentSessionNumber,
+  db
 }) => {
+  // Check if someone disarmed the alarm for this session
+  let disarmed
+  try {
+    const savedState = JSON.parse(await db.get(NO_LIST_SUBMITTED))
+    disarmed =
+      savedState.disarmed &&
+      Number(savedState.currentSessionNumber) ===
+        currentSessionNumber.toNumber()
+  } catch (err) {
+    if (err.type !== 'NotFoundError') throw new Error(err)
+  }
+  if (disarmed) return
+
   const sessionEnd = lastApprovalTime.add(submissionTimeout).toNumber()
 
   // This bot will submit an empty list if we are one hour or less
@@ -17,6 +43,8 @@ module.exports = async ({
 
   if (Date.now() / 1000 > sessionEnd) return // Wait for the next session.
 
+  console.info('Within list submission period')
+
   let submittedListIndexes
   let submissionDeposit
   try {
@@ -29,9 +57,37 @@ module.exports = async ({
     throw err
   }
 
-  // Check if no one submitted a list.
-  if (submittedListIndexes.length > 0) return
+  // Check if one of the whitelisted addresses submitted a list.
+  let submittedLists
+  try {
+    submittedLists = await Promise.all(
+      submittedListIndexes.map(i => governor.submissions(i.toString()))
+    )
+  } catch (err) {
+    console.error(
+      `Error fetching submissions. Indexes ${submittedListIndexes.map(i =>
+        i.toString()
+      )}`
+    )
+    throw err
+  }
 
+  const submitterAddresses = JSON.parse(
+    process.env.SUBMITTER_ADDRESSES
+  ).map(submitter => getAddress(submitter))
+
+  if (
+    submittedLists
+      .map(({ submitter }) => getAddress(submitter))
+      .some(submitter => submitterAddresses.includes(submitter))
+  ) {
+    console.info('One of the whitelisted addresses submitted a list.')
+    return
+  }
+
+  console.info(
+    'Alarm is not disarmed, submission threshold passed and none of SUBMITTER_ADDRESSES sent a list.'
+  )
   console.info('Submitting empty list...')
 
   await governor.submitList([], [], [], [], '', {
